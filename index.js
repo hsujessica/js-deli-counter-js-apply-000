d9f10129822adc694295f9fad54d1bf6a54d039a
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-02-18 01:12:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length != 0) {
     return "Currently serving " + katzDeliLine[0] + ".";
     katzDeliLine.shift();
@@ -13,7 +13,7 @@ function nowServing() {
   return "There is nobody waiting to be served!";
 }
 
-function currentLine() {
+function currentLine(katzDeliLine) {
   if (katzDeliLine.length != 0) {
     var line = "The line is currently: ";
     for (let i = 0; i<katzDeliLine.length; i++) {
