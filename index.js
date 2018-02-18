7381e0c143a5a29b3413a2ae54207edaa464c59d
Jessica Hsu <23249301+hsujessica@users.noreply.github.com>
2018-02-18 01:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ function nowServing() {
 }
 
 function currentLine() {
-  if (katzDeliLine.length == 0) {
-    return "The line is currently empty."
-  }
-  var line = "The line is currently: ";
-  for (let i = 0; i<katzDeliLine.length; i++) {
-    line += Number.toString(i + 1) + ". " + katzDeliLine[i];
-    if (i < katzDeliLine.length - 1) {
-      line += ",";
+  if (katzDeliLine.length != 0) {
+    var line = "The line is currently: ";
+    for (let i = 0; i<katzDeliLine.length; i++) {
+      line += Number.toString(i + 1) + ". " + katzDeliLine[i];
+      if (i < katzDeliLine.length - 1) {
+        line += ",";
+      }
     }
+    return line;
   }
-  return line;
+  return "The line is currently empty."
 }
